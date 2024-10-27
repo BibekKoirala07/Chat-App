ad52a39dea5170677038cf513f4105bce8650b4b
Bibek Koirala <www.koiralabibek2058@gmail.com>
2024-10-27 19:05:42 +0545

    e

--- Server/models/userModel.js
+++ Server/models/userModel.js
@@ -35,7 +35,7 @@ userSchema.statics.registerStatics = async function (
     throw createError(400, "Please fill in all fields");
   }
 
-  const isAlreadyRegistered = await this.findOne({ email });
+  const isAlreadyRegistered = await this.findOne({ email, isVerified: true });
   if (isAlreadyRegistered) {
     throw createError(
       409,
